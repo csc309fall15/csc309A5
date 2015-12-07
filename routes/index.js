e9d40528926a2a1668c1e96825fabd8308cf7ad8
josephlee427 <josephl@live.ca>
2015-12-06 22:20:40 -0500

    facebook login, not complete
    cant do facebook login cause we have no app tokens

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,36 @@ var Account = require('../models/account');
 var Trade = require('../models/trade');
 var mongoose = require('mongoose');
 var router = express.Router();
+var FacebookStrategy = require('passport-facebook').Strategy
 
 var app = express();
 
 var multer  = require('multer');
 var upload = multer({ dest: './public/images' });
+
+var FACEBOOK_APP_ID = "--Insert app here if we had one--";
+var FACEBOOK_APP_SECRET = "--Client secret strings--";
+
+
+// Connect to facebook ID for a login
+// Go to the callback if authentication fails
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Facebook profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Facebook account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
 
 // GET home page.
 router.get('/', function(req, res) {
@@ -383,5 +408,29 @@ router.post('/upload', upload.single('image'), function(req, res) {
 router.get('/ping', function(req, res) {
     res.status(200).send("pong!");
 });
+
+
+// GET /auth/facebook
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Facebook authentication will involve
+//   redirecting the user to facebook.com.  After authorization, Facebook will
+//   redirect the user back to this application at /auth/facebook/callback
+router.get('/auth/facebook',
+    passport.authenticate('facebook'),
+    function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+    });
+
+// GET /auth/facebook/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
 
 module.exports = router;
